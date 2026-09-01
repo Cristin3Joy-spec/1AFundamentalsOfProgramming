7dac002487e6676710a1b18fb7d437cc7853fb6e
Cristin3Joy <quijanocristinejoy24@gmail.com>
2026-09-01 23:07:02 +0800

    Assignment 3

---
+++ Assignment3.java
@@ -0,0 +1,20 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		//Personal Information 
+String firstname = "Cristine joy";
+String lastname = "Quijano";
+String address = "Cuyapo, Nueva Ecija";
+String mobilenumber = "09265929234";
+int age = 18;
+int gwa = 96;
+
+      //Output Display
+      System.out.println("First Name: " + firstname);
+      System.out.println("Last Name: " + lastname);
+      System.out.println("Address: " + address);
+      System.out.println("Mobile Number: " +mobilenumber);
+      System.out.println("Age: " + age);
+      System.out.println("GWA: " + gwa);
+	}
+}
